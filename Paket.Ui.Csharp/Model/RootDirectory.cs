1a7b0e8d1df72f41d82f32fa8e6f35970a2ca854
Johan Larsson <johan_gavle1@hotmail.com>
2016-07-29 22:54:33 +0200

    Make hack designtime only.

--- Paket.Ui.Csharp/Model/RootDirectory.cs
+++ Paket.Ui.Csharp/Model/RootDirectory.cs
@@ -1,16 +1,25 @@
 ﻿namespace Paket.Ui.Csharp
 {
+    using System.ComponentModel;
     using System.IO;
+    using System.Windows;
 
     public static class RootDirectory
     {
+        private static readonly DependencyObject DependencyObject = new DependencyObject();
+
         // Using global mutable state here, why not?
         public static DirectoryInfo Current { get; set; } = DesigntimeDirectory();
 
         private static DirectoryInfo DesigntimeDirectory()
         {
-            // Hacking it quick and dirty for now.
-            return new DirectoryInfo(@"C:\Git\Third Party\Paket.VisualStudio");
+            if (DesignerProperties.GetIsInDesignMode(DependencyObject))
+            {
+                // Hacking it quick and dirty for now.
+                return new DirectoryInfo(@"C:\Git\Third Party\Paket.VisualStudio");
+            }
+
+            return null;
         }
     }
 }
