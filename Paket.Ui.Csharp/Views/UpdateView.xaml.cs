3cb39dbf2ab06b57945e68440da0cff3abe0efbb
Johan Larsson <johan_gavle1@hotmail.com>
2016-07-31 21:02:18 +0200

    WIP scripting trying to fetch packages from cache.

---
+++ Paket.Ui.Csharp/Views/UpdateView.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Paket.Ui.Csharp
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for UpdateView.xaml
+    /// </summary>
+    public partial class UpdateView : UserControl
+    {
+        public UpdateView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Paket.VisualStudio.Core/Nuget.Cache.fsx
@@ -0,0 +1,12 @@
+﻿#r "System.dll"
+#r "./packages/paket"
+open System
+open System.IO
+
+let cacheDirectory =
+    let dir = System.Environment.GetFolderPath System.Environment.SpecialFolder.LocalApplicationData
+    System.IO.Path.Combine(dir, "NuGet", "Cache")
+
+let files = Directory.EnumerateFiles(cacheDirectory, "*.nupkg") |> Array.ofSeq
+
+Paket
