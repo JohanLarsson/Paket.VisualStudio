e691436bc5898f2b2c4ce15512c9de7b7f752455
Johan Larsson <johan_gavle1@hotmail.com>
2016-08-01 00:39:24 +0200

    Getting list of packages out of local cache now.
    - Need to filter out r# packages.

--- Paket.VisualStudio.Core/Nuget.Cache.fsx
+++ Paket.VisualStudio.Core/Nuget.Cache.fsx
@@ -1,12 +1,20 @@
-﻿#r "System.dll"
-#r "./packages/paket"
+﻿#r "System"
+#r "../packages/Paket.Core/lib/net45/Paket.Core.dll"
 open System
 open System.IO
+open System.Text.RegularExpressions
 
-let cacheDirectory =
-    let dir = System.Environment.GetFolderPath System.Environment.SpecialFolder.LocalApplicationData
-    System.IO.Path.Combine(dir, "NuGet", "Cache")
+let splitNameAndVersion (fileName : string) = 
+    let m = Regex.Match(fileName, @"(?<name>.+)\.(?<version>\d+(\.\d+){0,3})", RegexOptions.RightToLeft ||| RegexOptions.ExplicitCapture)
+    if m.Success then
+        let name = Paket.Domain.PackageName m.Groups.["name"].Value
+        let version = Paket.SemVer.Parse m.Groups.["version"].Value
+        Some(name, version)
+    else None
 
-let files = Directory.EnumerateFiles(cacheDirectory, "*.nupkg") |> Array.ofSeq
-
-Paket
+let files = Directory.EnumerateFiles(Paket.Constants.NuGetCacheFolder, "*.nupkg")
+            |> Seq.map Path.GetFileNameWithoutExtension
+            |> Seq.choose splitNameAndVersion
+            |> Seq.groupBy fst
+            |> Seq.map (fun (_, values) -> values |> Seq.maxBy snd )
+            |> List.ofSeq
