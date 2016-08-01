befe1a2f24ffd8aafcf97d869654546f25e288d4
Johan Larsson <johan_gavle1@hotmail.com>
2016-08-01 09:48:18 +0200

    Start getting stuff from nuget.

--- Paket.VisualStudio.Core/Scripts/Jetbrains.Feed.fsx
+++ Paket.VisualStudio.Core/Scripts/Jetbrains.Feed.fsx
@@ -3,14 +3,15 @@
 
 open System
 open System.Xml.Linq
-let getPackageNames (doc : XDocument) =
-    doc.Root.Elements()
-    |> Seq.filter(fun e -> e.Name.LocalName ="entry")
-    |> Seq.map (fun e -> e.Elements() |> Seq.find (fun e -> e.Name.LocalName = "title"))
-    |> Seq.map (fun e -> e.Value)
-    |> Seq.distinct
 
 let getAllResharperPackages =
+    let getPackageNames (doc : XDocument) =
+        doc.Root.Elements()
+        |> Seq.filter(fun e -> e.Name.LocalName ="entry")
+        |> Seq.map (fun e -> e.Elements() |> Seq.find (fun e -> e.Name.LocalName = "title"))
+        |> Seq.map (fun e -> e.Value)
+        |> Seq.distinct
+
     let urls = [
                    "https://resharper-plugins.jetbrains.com/api/v2/curated-feeds/Wave_v1.0/Packages"
                    "https://resharper-plugins.jetbrains.com/api/v2/curated-feeds/Wave_v2.0/Packages"
@@ -22,7 +23,7 @@ let getAllResharperPackages =
     urls
     |> Seq.map XDocument.Load
     |> Seq.map getPackageNames
-    |> Seq.collect (fun x -> x)
+    |> Seq.collect id
     |> Seq.distinct
 
 System.IO.File.WriteAllLines("C:\Temp\paket.exclude", getAllResharperPackages)
--- Paket.VisualStudio.Core/Scripts/Nuget.fsx
+++ Paket.VisualStudio.Core/Scripts/Nuget.fsx
@@ -1,21 +1,13 @@
 ﻿#r "System.dll"
+#r "../../packages/Paket.Core/lib/net45/Paket.Core.dll"
+#r "../../packages/Newtonsoft.Json/lib/net45/Newtonsoft.Json.dll"
 
 open System
-open System.Net
+open Paket
 
-let download (url:string) =
-    printfn "%s" url;
-    try
-      use client = new WebClient()
-      let proxy = WebRequest.DefaultWebProxy
-      proxy.Credentials <- CredentialCache.DefaultCredentials
-      client.Proxy <- proxy
-      let result = client.DownloadString url
-      printf "%s" result
-    with
-    | ex -> printfn "Exception! %s " (ex.Message)
+let catalog = Paket.NuGetV3.getCatalog Paket.Constants.DefaultNuGetV3Stream None |> Async.RunSynchronously
 
 //download "http://www.nuget.org/api/v2/Packages"
 //download "http://api.nuget.org/v3/index.json"
 //download "http://api.nuget.org/v3/catalog0/index.json"
-download "http://api.nuget.org/v3/Json.Net.json"
+//download "http://api.nuget.org/v3/Json.Net.json"
