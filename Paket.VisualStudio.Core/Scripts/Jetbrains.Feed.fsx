4ac5ee91ed388951d66ec269c8594a0367ba0316
Johan Larsson <johan_gavle1@hotmail.com>
2016-08-01 13:34:19 +0200

    Follow links in Jetbrains.Feed

--- Paket.VisualStudio.Core/Scripts/Jetbrains.Feed.fsx
+++ Paket.VisualStudio.Core/Scripts/Jetbrains.Feed.fsx
@@ -9,40 +9,63 @@ open Paket
 open Paket.PackageSources
 open System
 open System.Xml.Linq
-fsi.PrintLength <- 1000
 
-let getAllResharperPackages =
-    let getPackageNames (doc : XDocument) =
+//fsi.PrintLength <- 1000
+let getAllResharperPackages () = 
+    let getPackageNames (doc : XDocument) = 
         doc.Root.Elements()
-        |> Seq.filter(fun e -> e.Name.LocalName ="entry")
+        |> Seq.filter (fun e -> e.Name.LocalName = "entry")
         |> Seq.map (fun e -> e.Elements() |> Seq.find (fun e -> e.Name.LocalName = "title"))
         |> Seq.map (fun e -> e.Value)
         |> Seq.distinct
+        |> List.ofSeq
+    
+    let tryGetAttribute (e : XElement) name = 
+        if e.HasAttributes then e.Attributes() |> Seq.tryFind (fun a -> a.Name.LocalName = name)
+        else None
+    
+    let tryGetNext (e : XElement) = 
+        if e.HasAttributes then 
+            let link = e.Elements() |> Seq.tryFindBack (fun e -> e.Name.LocalName = "link")
+            match link with
+            | Some e -> 
+                let next = (tryGetAttribute e "next", tryGetAttribute e "href")
+                match next with
+                | (Some _, Some a) -> Some a.Value
+                | _, _ -> None
+            | None -> None
+        else None
+    
+    let rec getAllPackages (url : string) packages = 
+        let doc = XDocument.Load url
+        let packages = packages |> List.append (getPackageNames doc)
+        let next = tryGetNext doc.Root
+        match next with
+        | Some url -> List.append packages (getAllPackages url packages)
+        | None -> packages
+    
     let isNuget package = 
         let source = PackageSource.NuGetV3Source Constants.DefaultNuGetV3Stream
         let packages = NuGetV3.FindPackages(source.Auth, source.Url, package, 1) |> Async.RunSynchronously
         packages.Length > 0
-
-    let urls = [
-                   "https://resharper-plugins.jetbrains.com/api/v2/curated-feeds/Wave_v1.0/Packages"
-                   "https://resharper-plugins.jetbrains.com/api/v2/curated-feeds/Wave_v2.0/Packages"
-                   "https://resharper-plugins.jetbrains.com/api/v2/curated-feeds/Wave_v3.0/Packages"
-                   "https://resharper-plugins.jetbrains.com/api/v2/curated-feeds/Wave_v4.0/Packages"
-                   "https://resharper-plugins.jetbrains.com/api/v2/curated-feeds/Wave_v5.0/Packages"
-                   "https://resharper-plugins.jetbrains.com/api/v2/curated-feeds/Wave_v6.0/Packages"
-               ]
+    
+    let urls = 
+        [ "https://resharper-plugins.jetbrains.com/api/v2/curated-feeds/Wave_v1.0/Packages"; 
+          "https://resharper-plugins.jetbrains.com/api/v2/curated-feeds/Wave_v2.0/Packages"; 
+          "https://resharper-plugins.jetbrains.com/api/v2/curated-feeds/Wave_v3.0/Packages"; 
+          "https://resharper-plugins.jetbrains.com/api/v2/curated-feeds/Wave_v4.0/Packages"; 
+          "https://resharper-plugins.jetbrains.com/api/v2/curated-feeds/Wave_v5.0/Packages"; 
+          "https://resharper-plugins.jetbrains.com/api/v2/curated-feeds/Wave_v6.0/Packages" ]
     urls
-    |> Seq.map XDocument.Load
-    |> Seq.map getPackageNames
+    |> Seq.map (fun url -> getAllPackages url [])
     |> Seq.collect id
     |> Seq.distinct
-    |> Seq.filter(fun x -> not (isNuget x))
+    |> Seq.filter (fun x -> not (isNuget x))
     |> Seq.sort
 
-let packages = getAllResharperPackages
+let packages = getAllResharperPackages()
 
 for package in packages do
     printfn "\"%s\"" package
 
 System.IO.File.WriteAllLines(@"C:\Temp\paket.ignore", packages)
-
--- Paket.VisualStudio.Core/Scripts/Nuget.Cache.fsx
+++ Paket.VisualStudio.Core/Scripts/Nuget.Cache.fsx
@@ -11,47 +11,49 @@ let splitNameAndVersion (fileName : string) =
         let version = Paket.SemVer.Parse m.Groups.["version"].Value
         Some(name, version)
     else None
-let ignore = [
-"ActiveMesa.R2P.R10"
-"ActiveMesa.R2P.R9"
-AgentSmith
-AgentZorge.R10_0
-AgentZorge.R2016_1
-AgentZorge.R9_2
-Basrach.YouCantSpell
-Binbin.Exceptional.2016.2
-Bootstrap3.LiveTemplates
-Caliburn.Light.Annotations
-Catel.ReSharper.100
-Catel.ReSharper.90
-Catel.ReSharper.91
-Catel.ReSharper.92
-CitizenMatt.Clippy
-CitizenMatt.PreviewTab
-CitizenMatt.TemplateDescriptions
-CitizenMatt.Xunit
-Coconut.ReSharper
-CodeAnnotation.Pack
-Community.External.Annotations.R90
-ConfigureAwaitChecker.v9
-Drakmyth.ReSharper.Macros
-EggBlox.ReSharper.Plugins
-EngineeringTeam.Settings
-EtherealCode.Emmet.ReSharper
-EtherealCode.ReSpeller
-EtherealCode.ReSpellerPro
-Exceptional.2016.1
-Find.By
-JetBrains.XunitTemplates
-ReSharper.ImplicitNullability
-ReSharper.SerializationInspections
-ReSharper.XmlDocInspections
-ReshSettings.Discover
-campaignmonitor.microservice.templates
-]
+let ignore = Set [|
+                "ActiveMesa.R2P.R10"
+                "ActiveMesa.R2P.R9"
+                "AgentSmith"
+                "AgentZorge.R10_0"
+                "AgentZorge.R2016_1"
+                "AgentZorge.R9_2"
+                "Basrach.YouCantSpell"
+                "Binbin.Exceptional.2016.2"
+                "Bootstrap3.LiveTemplates"
+                "Caliburn.Light.Annotations"
+                "Catel.ReSharper.100"
+                "Catel.ReSharper.90"
+                "Catel.ReSharper.91"
+                "Catel.ReSharper.92"
+                "CitizenMatt.Clippy"
+                "CitizenMatt.PreviewTab"
+                "CitizenMatt.TemplateDescriptions"
+                "CitizenMatt.Xunit"
+                "Coconut.ReSharper"
+                "CodeAnnotation.Pack"
+                "Community.External.Annotations.R90"
+                "ConfigureAwaitChecker.v9"
+                "Drakmyth.ReSharper.Macros"
+                "EggBlox.ReSharper.Plugins"
+                "EngineeringTeam.Settings"
+                "EtherealCode.Emmet.ReSharper"
+                "EtherealCode.ReSpeller"
+                "EtherealCode.ReSpellerPro"
+                "Exceptional.2016.1"
+                "Find.By"
+                "JetBrains.XunitTemplates"
+                "ReSharper.ImplicitNullability"
+                "ReSharper.SerializationInspections"
+                "ReSharper.XmlDocInspections"
+                "ReshSettings.Discover"
+                "campaignmonitor.microservice.templates"
+            |]
+
 let files = Directory.EnumerateFiles(Paket.Constants.NuGetCacheFolder, "*.nupkg")
             |> Seq.map Path.GetFileNameWithoutExtension
             |> Seq.choose splitNameAndVersion
+            |> Seq.filter (fun ((name, _), _) -> not (ignore.Contains(name))) 
             |> Seq.groupBy fst
             |> Seq.map (fun (_, values) -> values |> Seq.maxBy snd )
             |> List.ofSeq
